0b9c39de4aeca796ced92296afd5e63abb3efd83
omgdyh <xiaohongbili@outlook.om>
2025-12-08 23:15:40 +0800

    创建了创造比赛文件夹的文件

---
+++ create.bat
@@ -0,0 +1,6 @@
+mkdir .\contest\%1
+mkdir .\contest\%1\data
+mkdir .\contest\%1\result
+mkdir .\contest\%1\source
+type example > .\contest\%1\result.json
+cls
---
+++ init.bat
@@ -0,0 +1,7 @@
+mkdir .\contest
+mkdir .\contest\self
+mkdir .\contest\self\data
+mkdir .\contest\self\source
+mkdir .\contest\self\result
+type example > .\contest\self\result.json
+cls
